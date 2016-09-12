24eb2615e8f6a8372b79753208d25ef3c98c2160
sahana kamath <sahanakamath@sahanas-MacBook-Pro.local>
2016-09-12 18:52:44 -0400

    Challenge_8, 1-3 Exercise Completed

---
+++ Challenge_8/challenge.js
@@ -0,0 +1,24 @@
+//EXERCISE #1 : print 1 to 100 in console
+for(var i=0;i<=100;i++){
+    console.log(i);
+}
+
+//EXERCISE #2: Pattern triangle
+for(var x="#";x.length<8;x=x+"#"){
+    console.log(x);
+}
+
+//EXERCISE #3: Fizz Buzz
+for (var i = 1; i <= 100; i++) {
+  var result = "";
+  if (i % 3 == 0)
+    result = "Fizz";
+  if (i % 5 == 0)
+    result = result + "Buzz";
+  console.log(result || i);
+}
+
+//EXERCISE #4: 8*8 Grid
+
+
+
