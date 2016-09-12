8fa297984868599f90b4f08a62f017ce204b5950
sahana kamath <sahanakamath@sahanas-MacBook-Pro.local>
2016-09-12 12:41:52 -0400

    Challenge_5 Completed

---
+++ Challenge_5/challenge.js
@@ -0,0 +1,39 @@
+//EXERCISE 1:
+var name = "Sahana Kamath";
+console.log("Hello "+ name);
+
+//EXERCISE 2:
+var x = 1125;
+var answer = x%2;
+console.log("Answer = " + answer);
+
+//EXERCISE 3:
+var a = true;
+var b = false;
+var c = true;
+console.log(a||b||c);
+
+//EXERCISE 4:
+var a = true;
+var b = false;
+var c = true;
+console.log(a && b && c);
+
+//EXERCISE 5:
+var answer1 = 10 - 3 * 5/4 + 202 % 3;
+var answer2 =  10 - (3 * (5/4) + (202 % 3));
+console.log("Answer1 = "+ answer1, "Answer2 = " + answer2);
+
+//EXERCISE 6:
+var num1 = 12;
+var n = num1.toString();
+
+var str1 = "113";
+var num2 = parseInt(str1);
+
+var str2 = "1.12356";
+var flt1 = parseFloat(str2);
+
+var str3 = "Hello World";
+var x = parseInt(str3);
+console.log("x = " + x);
