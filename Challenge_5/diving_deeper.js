f4a22ff3dc53265729bda644e35ffb74c0975bd8
sahana kamath <sahanakamath@sahanas-MacBook-Pro.local>
2016-09-12 12:58:02 -0400

    Challenge_5 and Diving Deeper Completed

---
+++ Challenge_5/diving_deeper.js
@@ -0,0 +1,11 @@
+//Part 1:
+var str1 = "I am a BrainStation student";
+var str2 = "I am a student";
+var result1 = str1.search("BrainStation");
+var result2 = str2.search("BrainStation");
+console.log("Result1 = " + result1 + " " + "Result2 = " + result2 );
+
+//Part 2:
+var str = "I am a BrainStation student";
+var result = str.replace("BrainStation student","Junior Developer");
+console.log("Str = " + str + "\n "+ "Replaced Result = " + result);
